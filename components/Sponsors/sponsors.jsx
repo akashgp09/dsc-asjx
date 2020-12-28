80ce7ddc93c215bf0d5e25ef111e2fb3df79bf49
Akash Gupta <akashgp9@gmail.com>
2020-12-28 18:50:30 +0530

    Created Sponsors Section
    Signed-off-by: Akash Gupta <akashgp9@gmail.com>

---
+++ components/Sponsors/sponsors.jsx
@@ -0,0 +1,77 @@
+import styles from "./sponsors.module.css";
+export default function Sponsors() {
+  return (
+    <>
+      <div className={styles.background} id="sponsors-section">
+        <div className={styles.header}>
+          <span className={styles.half_background}>Sponsors</span>
+        </div>
+        <div className={styles.sponsor_type}>Platinum Sponsors</div>
+        <div className="container text-center">
+          <div className="row">
+            <div className={`col-md-4  ${styles.sponsor_img_wrapper}`}>
+              <img
+                src="/images/sponsors/devfolio.png"
+                className={styles.sponsor_img_size}
+              />
+            </div>
+            <div
+              className={`col-md-4  ${styles.sponsor_img_wrapper} align-self-center`}
+            >
+              <img
+                src="/images/sponsors/cn.png"
+                className={styles.sponsor_img_size}
+              />
+            </div>
+            <div
+              className={`col-md-4  ${styles.sponsor_img_wrapper} align-self-end`}
+            >
+              <img
+                src="/images/sponsors/cb.png"
+                className={styles.sponsor_img_size}
+              />
+            </div>
+          </div>
+        </div>
+        <div className={styles.sponsor_type}>Gold Sponsors</div>
+        <div className="container text-center">
+          <div className="row">
+            <div className={`col-md-4  ${styles.sponsor_img_wrapper}`}>
+              <img
+                src="/images/sponsors/t.png"
+                className={styles.sponsor_imgbig_size}
+              />
+            </div>
+            <div
+              className={`col-md-4  ${styles.sponsor_img_wrapper} align-self-center`}
+            >
+              <img
+                src="/images/sponsors/m.png"
+                className={styles.sponsor_imgbig_size}
+              />
+            </div>
+            <div
+              className={`col-md-4  ${styles.sponsor_img_wrapper} align-self-end`}
+            >
+              <img
+                src="/images/sponsors/portis.png"
+                className={styles.sponsor_img_size}
+              />
+            </div>
+          </div>
+        </div>
+        <div className={styles.sponsor_type}>Silver Sponsors</div>
+        <div className="container text-center">
+          <div className="row">
+            <div className={`col-md-4  ${styles.sponsor_img_wrapper}`}>
+              <img
+                src="/images/sponsors/hc.png"
+                className={styles.sponsor_img_size}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
