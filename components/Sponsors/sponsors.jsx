28d6c9e96693c39010a90b51ff0a3880f3dd625b
Akash Gupta <akashgp9@gmail.com>
2020-12-30 11:54:10 +0530

    updated sponsors section

--- components/Sponsors/sponsors.jsx
+++ components/Sponsors/sponsors.jsx
@@ -6,20 +6,25 @@ export default function Sponsors() {
         <div className={styles.header}>
           <span className={styles.half_background}>Sponsors</span>
         </div>
-        <div className={styles.sponsor_type}>Platinum Sponsors</div>
+        <div className={styles.sponsor_type}>Title Sponsor</div>
         <div className="container text-center">
-          <div className="row">
-            <div className={`col-md-4  ${styles.sponsor_img_wrapper}`}>
-              <img
-                src="/images/sponsors/devfolio.png"
-                className={styles.sponsor_img_size}
-              />
-            </div>
+          <div className="row justify-content-center">
             <div
               className={`col-md-4  ${styles.sponsor_img_wrapper} align-self-center`}
             >
               <img
                 src="/images/sponsors/cn.png"
+                className={styles.sponsor_img_size}
+              />
+            </div>
+          </div>
+        </div>
+        <div className={styles.sponsor_type}>Platinum Sponsors</div>
+        <div className="container text-center">
+          <div className="row justify-content-center">
+            <div className={`col-md-4  ${styles.sponsor_img_wrapper}`}>
+              <img
+                src="/images/sponsors/devfolio.png"
                 className={styles.sponsor_img_size}
               />
             </div>
@@ -60,9 +65,9 @@ export default function Sponsors() {
             </div>
           </div>
         </div>
-        <div className={styles.sponsor_type}>Silver Sponsors</div>
+        <div className={styles.sponsor_type}>Silver Sponsor</div>
         <div className="container text-center">
-          <div className="row">
+          <div className="row justify-content-center">
             <div className={`col-md-4  ${styles.sponsor_img_wrapper}`}>
               <img
                 src="/images/sponsors/hc.png"
