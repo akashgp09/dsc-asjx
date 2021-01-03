5c3926e1af923175033833b95564d20588e8cc54
Akash Gupta <akashgp9@gmail.com>
2021-01-03 11:29:21 +0530

    Syncronized navbar scroll

--- components/Intro/intro.jsx
+++ components/Intro/intro.jsx
@@ -93,7 +93,21 @@ export default function Intro() {
           </span>
         </div>
         <div className="row">
-          <div className={styles.android}><img src="/images/android.gif" height="250" width="270" className={styles.img}/>KIIT's largest Android Hackathon<img src="/images/android2.gif" height="250" width="270" className={styles.img}/></div>
+          <div className={styles.android}>
+            <img
+              src="/images/android.gif"
+              height="250"
+              width="270"
+              className={styles.img}
+            />
+            KIIT's largest Android Hackathon
+            <img
+              src="/images/android2.gif"
+              height="250"
+              width="270"
+              className={styles.img}
+            />
+          </div>
           <div className={styles.date}>
             11th January 2021 - 22nd January 2021
           </div>
