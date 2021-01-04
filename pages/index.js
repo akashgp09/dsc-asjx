4437def650f436462c89e045cd44ac924077fc04
Arpit Mohapatra <arpit.mohapatra19@gmail.com>
2021-01-04 11:21:33 +0530

    Prizes colour changed

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/asjx-logo.ico" />
         <meta name="description" content="DSC ASJ-X" />
         <meta
           property="og:image"
